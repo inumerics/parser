add9bfd322d8a552bdc88bcd92c9c2a1c48bdf87
inumerics <island.numerics@gmail.com>
2022-06-18 20:16:34 -1000

    Adding the finite class.

---
+++ parser/finite.hpp
@@ -0,0 +1,85 @@
+/**
+ * The state of a finite state machine for recognizing terminals, such as words
+ * and numbers, in an input string.
+ */
+
+#ifndef finite_hpp
+#define finite_hpp
+
+#include <set>
+#include <string>
+#include <vector>
+#include <iostream>
+
+/**
+ * Name of a terminal to find.  The rank is required when multiple terminals
+ * match a string and the one with the lowest rank is accepted.
+ */
+class Term {
+  public:
+    Term(const std::string& name, size_t rank);
+    std::string name;
+    size_t rank;
+};
+
+/**
+ * The state machine is constructed by connecting states to each other with
+ * outputs.  Each state contains a vector of outputs that defines the next
+ * active state for a given input character.  While operating the machine, one
+ * or more states is in a set of active states.
+ */
+class Finite
+{
+  public:
+    Finite();
+    Finite(Term* term);
+    
+    /** Pointer indicates a match when this state active. */
+    Term* term;
+
+    /** Checks an input stream for a match starting from this state. */
+    Term* scan(std::istream& in, std::string* match);
+    
+    /**
+     * If the next character is within the output range of an active state, then
+     * the target state of that output is added to the new set of active states.
+     * Empty, or epsilon, outputs provide optional patterns.
+     */
+    class Out {
+      public:
+        Out(int first, int last, Finite* next);
+        Out(Finite* next);
+        
+        Finite* next;
+        bool is_epsilon();
+        bool in_range(char c);
+        
+        bool epsilon;
+        int first;
+        int last;
+    };
+    
+    /** Builds and returns new outputs, but retains ownership. */
+    Out* add_out(int c, Finite* next);
+    Out* add_out(int first, int last, Finite* next);
+    Out* add_epsilon(Finite* next);
+        
+    /** Finds output targets with the given character in its range. */
+    void find_next(int c, std::set<Finite*>* next) const;
+        
+    /** Follows empty transitions until no new states are found. */
+    static void closure(std::set<Finite*>* states);
+    void closure(std::set<Finite*>* states, std::vector<Finite*>* stack) const;
+    
+    /** Determines the accepted match in cases with multiple final states. */
+    static bool lower_rank(const Finite* left, const Finite* right);
+    
+  private:
+    
+    /** Next active states for a given input character. */
+    std::vector<std::unique_ptr<Out>> outs;
+    
+};
+
+#endif
+
--- parser/main.cpp
+++ parser/main.cpp
@@ -1,8 +1,58 @@
-#include <iostream>
+/**
+ * Finite state machine for recognizing words and numbers in an input string.
+ */
+
+#include "finite.hpp"
+
+#include <sstream>
 
 int
-main(int argc, const char * argv[])
+main(int argc, const char* argv[])
 {
-    std::cout << "Aloha!\n";
+    /** Finding words and numbers. */
+    Term word("word", 0);
+    Term number("number", 1);
+    
+    /** States for each terminal and the negative sign. */
+    Finite start;
+    Finite state_word(&word);
+    Finite state_number(&number);
+    Finite state_sign;
+    
+    /** Add outputs to construct the state machine. */
+    start.add_out('a', 'z', &state_word);
+    start.add_out('-', &state_sign);
+
+    /** An empty output is added to bypass the optional negative sign. */
+    start.add_epsilon(&state_sign);
+
+    /**
+     * Circle back to the previous state so that continuing to see a letter
+     * while in the word state, or a digit while in the number state, keeps
+     * those states in the active set.
+     */
+    state_word.add_out('a', 'z', &state_word);
+    state_sign.add_out('0', '9', &state_number);
+    state_number.add_out('0', '9', &state_number);
+    
+    std::stringstream example("hello test 123");
+    
+    /** Scan identifies terminals in the input. */
+    while (true) {
+        example >> std::ws;
+        if (example.peek() == EOF) {
+            break;
+        }
+        
+        std::string match;
+        Term* term = start.scan(example, &match);
+
+        if (term) {
+            std::cout << match << " is a " << term->name << ".\n";
+        } else {
+            std::cin >> match;
+        }
+    }
+
     return 0;
 }
