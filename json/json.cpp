e987adc8dc6d8d8d214f96028a3678858a6c993b
inumerics <island.numerics@gmail.com>
2023-03-08 20:03:01 -1000

    Adding the Json example.

--- json/json.cpp
+++ json/json.cpp
@@ -170,3 +170,188 @@ values_value(Table* table, unique_ptr<JsonArray>& E1,
     E1->values.push_back(std::move(E2));
     return std::move(E1);
 }
+
+/******************************************************************************/
+void
+Parser::start()
+{
+    this->node = &node0;;
+    this->text.clear();
+    
+    states.clear();
+    symbols.clear();
+    values.clear();
+    
+    states.push_back(&state0);
+    symbols.push_back(nullptr);
+    values.push_back(nullptr);
+};
+
+/**
+ * The calculator calls scan with the next input character to match terminals in
+ * input.  If a terminal is found,  the calculator updates the stack based on
+ * the action in the parse table.  Based on the action in the parse table, the
+ * new symbol is either shifted onto the stack, or the stack is reduced by a
+ * rule and the user defined action is called with the values on top of the
+ * stack as arguments.  This process is continued until the end of the input at
+ * which point the end mark symbol is used to reduce the remaining symbols still
+ * of the stack into a single value.  The following function reads an input one
+ * character at a time until a terminal is found.  When a symbol is found the
+ * function calls another function to update the state of the calculator.
+ */
+bool
+Parser::scan(Table* table, int c)
+{
+    while (true)
+    {
+        if (node == &node0 && isspace(c)) {
+            return true;
+        }
+        else {
+            Node* next = node_next(node, c);
+            
+            if (next) {
+                text.push_back(c);
+                node = next;
+                return true;
+            }
+            else {
+                if (!node_accept(node)) {
+                    return false;
+                }
+                
+                Value* value = node_scan(node, table, text);
+                if (!advance(table, node_accept(node), value)) {
+                    return false;
+                }
+                node = &node0;
+                text.clear();
+            }
+        }
+    }
+}
+
+bool
+Parser::scan_end(Table* table)
+{
+    Symbol* accept = node_accept(node);
+    
+    if (accept) {
+        Value* value = node_scan(node, table, text);
+        if (!advance(table, accept, value)) {
+            return false;
+        }
+    } else if (node != &node0) {
+        return false;
+    }
+    
+    // TODO Check for accept.
+    if (!advance(table, &endmark, nullptr)) {
+        return false;
+    }
+    return true;
+};
+
+/**
+ * After a terminal is found in the input, it is either shifted onto the top of
+ * the stack or the stack is reduced by one of the predefined rules in the
+ * grammar.  If the stack is reduced, the corresponding user defined function is
+ * called with the values on the top of the stack as arguments.
+ */
+bool
+Parser::advance(Table* table, Symbol* sym, Value* val)
+{
+    while (true)
+    {
+        State* top = states.back();
+        State* next = find_shift(top, sym);
+        if (next) {
+            push(next, sym, val);
+            return true;
+        }
+        
+        
+        bool accept = false;
+        Rule* rule = find_reduce(top, sym, &accept);
+        
+        if (rule) {
+            size_t length = 0;
+            Symbol* nonterm = rule_nonterm(rule, &length);
+            
+            
+            Value* result = rule_reduce(rule, table, values.data() + values.size());
+            pop(length);
+            
+            top = states.back();
+            State* found = find_goto(top, nonterm);
+            push(found, nonterm, result);
+        } else {
+            return false;
+        }
+        
+        if (accept) {
+            return true;
+        }
+    }
+};
+
+/******************************************************************************/
+void
+Parser::push(State* state, Symbol* sym, Value* val)
+{
+    states.push_back(state);
+    symbols.push_back(sym);
+    values.push_back(val);
+}
+
+void
+Parser::pop(size_t count)
+{
+    for (size_t i = 0; i < count; i++) {
+        states.pop_back();
+        symbols.pop_back();
+        values.pop_back();
+    }
+};
+
+/******************************************************************************/
+int
+main(int argc, const char * argv[])
+{
+    if (argc != 2) {
+        std::cerr << "Expected a single input string.\n";
+        return 1;
+    }
+    
+    // TODO Just build on the stack.
+    auto parser = std::make_unique<Parser>();
+    parser->start();
+    if (!parser) {
+        std::cerr << "Error while building parser.\n";
+        return 1;
+    }
+    
+    Table table;
+    
+    std::stringstream in(argv[1]);
+    
+    while (true) {
+        int c = in.get();
+        if (c == EOF) {
+            bool ok = parser->scan_end(&table);
+            if (!ok) {
+                std::cerr << "Unexpected end of the input.\n";
+                return 1;
+            }
+            break;
+        } else {
+            bool ok = parser->scan(&table, c);
+            if (!ok) {
+                std::cerr << "Unexpected character.\n";
+                return 1;
+            }
+        }
+    }
+    
+    return 0;
+}
--- json/json.hpp
+++ json/json.hpp
@@ -25,45 +25,47 @@ class JsonObject;
 class JsonArray;
 
 /******************************************************************************/
-class JsonObject : public JsonValue {
-public:
+class JsonObject : public JsonValue
+{
+  public:
     std::map<std::string, std::unique_ptr<JsonValue>> values;
     
     bool has_key(const std::string& name) const;
     
     JsonObject* get_object(const std::string& name) const;
-    JsonArray*  get_array( const std::string& name) const;
+    JsonArray*  get_array (const std::string& name) const;
     std::string get_string(const std::string& name) const;
-    bool        get_bool(  const std::string& name) const;
+    bool        get_bool  (const std::string& name) const;
 };
 
 class JsonMember : public Value {
-public:
+  public:
     std::string name;
     std::unique_ptr<JsonValue> value;
 };
 
 /******************************************************************************/
-class JsonArray : public JsonValue {
-public:
+class JsonArray : public JsonValue
+{
+  public:
     std::vector<std::unique_ptr<JsonValue>> values;
 };
 
 /******************************************************************************/
 class JsonString : public JsonValue {
-public:
+  public:
     JsonString(const std::string& value);
     std::string value;
 };
 
 class JsonNumber : public JsonValue {
-public:
+  public:
     JsonNumber(double value);
     double value;
 };
 
 class JsonBool : public JsonValue {
-public:
+  public:
     JsonBool(bool value);
     bool value;
 };
@@ -73,24 +75,89 @@ class JsonNull : public JsonValue {
 
 /******************************************************************************/
 class Table {
-public:
+  public:
     std::unique_ptr<JsonValue> value;
 };
 
+/*****************************************************************************
+ * Classes defined by the state machine to parse a user defined grammar.  The
+ * rules of the grammar are written as a nonterminal followed by zero or more
+ * symbols.  The parser generates a set of nodes and states for identifing
+ * symbols in a string and reducing the input symbols by rules of the grammar.
+ */
+struct Symbol;
+struct Rule;
+
+struct Node;
+extern Node node0;
+
+struct State;
+extern State state0;
+
+extern Symbol endmark;
+
+/*****************************************************************************
+ * Function defined by the lexer for identifing terminals in an input string.
+ * Node next determines the next node for a
+ * given input character.  If no node is returned, check the current node to
+ * determine the accepted symbol for the input string.  With the accepted
+ * symbol, call scan to get the value for the symbol.
+ */
+Node*   node_next(Node* node, int c);
+Symbol* node_accept(Node* node);
+Value*  node_scan(Node* node, Table*, const std::string&);
+
+/*****************************************************************************
+ * Function defined by the parser.  For each new symbol, the parser determines
+ * if the symbol should be pushed onto the stack, or reduced by a rule of the
+ * grammar.  If the top symbols are reduced by a rule, call the reduce function
+ * to determine the new value for the top of the stack.  After reducing the
+ * rule, call goto to determine the next state of the parser.
+ */
+State* find_shift(State* state, Symbol* sym);
+Rule*  find_reduce(State* state, Symbol* sym, bool* accept);
+
+Symbol* rule_nonterm(Rule* rule, size_t* length);
+Value*  rule_reduce(Rule* rule, Table*, Value**);
+
+State* find_goto(State* state, Symbol* sym);
+
 /*******************************************************************************
- * This parser generator builds tables for a shift-reduce parser.  To include
- * the shift-reduce parser in another program, this parser class maintains a
- * stack of parse states, symbols and values.  This stack is updated based on
- * the the symbol just read from the input and the actions defined in the parse
- * table.
+ * With the grammar and the custom functions defined, the parser generator is
+ * run to build the table of actions.  However, two additional functions are
+ * needed to fully implement the calculator.  The first function reads the input
+ * string one character at a time looking for terminals.  The second function is
+ * passed the found terminal and based on the action table either adds the
+ * terminal to a stack, or reduces the stack by a grammar rule while calling its
+ * associated function.
  */
 class Parser {
-public:
-    static std::unique_ptr<Parser> build();
-    virtual ~Parser() = default;
+  public:
+    void start();
     
-    virtual bool scan(Table* table, int c) = 0;
-    virtual bool scan_end(Table* table) = 0;
+    /** Advance the parser with the next input character. */
+    bool scan(Table* table, int c);
+    
+    /** Advance the parser at the end of the input. */
+    bool scan_end(Table* table);
+    
+  private:
+    
+    /** State of the lexer and input characters of the current symbol. */
+    Node* node = &node0;
+    std::string text;
+    
+    /** Stack of the shift reduce parser. */
+    std::vector<State*>  states;
+    std::vector<Symbol*> symbols;
+    std::vector<Value*>  values;
+    
+    /** Utility methods for modifying to the stack. */
+    void push(State* state, Symbol* sym, Value* val);
+    void pop(size_t count);
+    
+    /** Either shift the symbol onto the stack, or reduce by a rule. */
+    bool advance(Table* table, Symbol* sym, Value* val);
 };
 
 #endif
